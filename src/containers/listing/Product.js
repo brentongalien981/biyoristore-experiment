e68e016e7b08c53dce7b7692e89342f32023a824
brentongalien981 <brentongalien981@gmail.com>
2021-01-31 20:59:12 -0500

    for "Conclusions", did "TASK: work on the display price of items"
    for "Conclusions", did "TASK: work on the display price of items"

--- src/containers/listing/Product.js
+++ src/containers/listing/Product.js
@@ -15,6 +15,18 @@ function Product(props) {
         return (<img src={url} key={i} alt="Image" />);
     });
 
+    const discountSellPrice = props.product.mostEfficientSeller.productSeller.discount_sell_price;
+    const sellPrice = props.product.mostEfficientSeller.productSeller.sell_price;
+
+    let priceComponent = (<span>{"$" + parseFloat(sellPrice).toFixed(2)}</span>);
+
+    if (discountSellPrice) {
+        const discountPriceStyle = { marginRight: "15%" };
+        priceComponent = (<span><s style={discountPriceStyle} className="text-muted">{"$" + parseFloat(sellPrice).toFixed(2)}</s>{" $" + parseFloat(discountSellPrice).toFixed(2)}</span>);
+    }
+
+
+
     return (
         <div className="product" onClick={(e) => props.onProductClicked(e, props, props.product)}>
             <figure className="product-image">
@@ -23,12 +35,10 @@ function Product(props) {
             <div className="product-meta">
                 <h3 className="product-title"><a href="#!" title={props.product.name}>{props.product.name}</a></h3>
                 <div className="product-price">
-                    <span>{"$" + props.product.price}</span>
-                    <span className="product-action">
-                        <a href="#!" onClick={(e) => props.onAddToCart(e, props.product)}>Add to cart</a>
-                    </span>
+                    {priceComponent}
+                    {/* <span className="product-action"><a href="#!" onClick={(e) => props.onAddToCart(e, props.product)}>Add to cart</a></span> */}
                 </div>
-                <a href="#!" className="product-like" onClick={(e) => props.onProductLiked(e)}></a>
+                {/* <a href="#!" className="product-like" onClick={(e) => props.onProductLiked(e)}></a> */}
             </div>
         </div>
     );
