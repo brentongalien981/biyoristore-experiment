49ffeb34e3307505aedbc6bf2a5e05a8186ddfe1
brentongalien981 <brentongalien981@gmail.com>
2021-02-06 00:20:30 -0500

    for "PP-01.1: app displays product-details", finishing it
    for "PP-01.1: app displays product-details", finishing it

--- src/containers/product/ProductDetails.js
+++ src/containers/product/ProductDetails.js
@@ -16,6 +16,11 @@ function ProductDetails(props) {
         priceSectionElement = (<span className="item-price"><s className="text-muted">${discountSellPrice}</s> ${sellPrice}</span>);
     }
 
+    let outOfStockComponent = null;
+    if (isProductOutOfStock(p)) {
+        outOfStockComponent = (<span className="item-price text-red">Out of stock</span>);
+    }
+
 
     return (
         <div className="col-lg-5 mb-5 mb-lg-0">
@@ -25,14 +30,8 @@ function ProductDetails(props) {
                     <span className="item-brand">{p?.brand?.name}</span>
                     <h1 className="item-title">{p?.name}</h1>
                     {priceSectionElement}
-                </div>
-            </div>
-
-
-            {/* product description */}
-            <div className="row">
-                <div className="col-12">
-                    <p>{p?.description}</p>
+                    <br />
+                    {outOfStockComponent}
                 </div>
             </div>
 
@@ -42,6 +41,35 @@ function ProductDetails(props) {
             <ProductActions product={p} />
         </div>
     );
+}
+
+
+
+function isProductOutOfStock(product) {
+    let returnVal = true;
+
+    switch (product.packageItemTypeId) {
+        case 1: // shirt
+        case 2: // jersey
+        case 3: // shorts
+        case 4: // hoodie
+        case 5: // shoes
+            const sizeAvailabilities = product.mostEfficientSeller?.sizeAvailabilities;
+            for (const sizeAvailability of sizeAvailabilities) {
+                if (sizeAvailability.quantity > 0) { 
+                    returnVal = false; 
+                    break;
+                }
+            }
+            
+            break;
+        case 6: // pctowercase
+            const quantity = product.mostEfficientSeller?.productSeller?.quantity;
+            if (quantity && quantity > 0) { returnVal = false; }
+            break;
+    }
+
+    return returnVal;
 }
 
 
