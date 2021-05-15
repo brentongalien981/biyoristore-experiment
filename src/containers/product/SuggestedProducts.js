574f71c58818047267449b5e346a963a10d973d4
brentongalien981 <brentongalien981@gmail.com>
2021-05-15 19:49:04 -0400

    current state
    current state

--- src/containers/product/SuggestedProducts.js
+++ src/containers/product/SuggestedProducts.js
@@ -12,24 +12,34 @@ function SuggestedProducts(props) {
 
 
     let mainSection = (
-        <div className="col">
-            <div className="tab-content" id="myTabContent">
-
-                <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                    <div className="owl-carousel owl-carousel-arrows visible" data-items="[4,4,2,1]" data-margin="10" data-loop="true" data-dots="true" data-nav="true">
-                        {relatedProducts}
-
-                    </div>
-                </div>
-
+        <>
+            <div className="col-12 mb-3">
+                <ul className="nav nav-tabs lavalamp" id="myTab" role="tablist">
+                    <li className="nav-item">
+                        <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Related Products</a>
+                    </li>
+                </ul>
             </div>
-        </div>
+
+            <div className="col">
+                <div className="tab-content" id="myTabContent">
+
+                    <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                        <div className="owl-carousel owl-carousel-arrows visible" data-items="[4,4,2,1]" data-margin="10" data-loop="true" data-dots="true" data-nav="true">
+                            {relatedProducts}
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
     );
 
 
     if (props.isReadingProduct) {
         mainSection = (
-            <div className="row gutter-2 gutter-lg-4 animate__animated animate__fadeIn">
+            <div className="col animate__animated animate__fadeIn">
                 <SpinnerLoader size='xl' />
             </div>
         );
@@ -40,19 +50,7 @@ function SuggestedProducts(props) {
         <section className="no-overflow">
             <div className="container">
                 <div className="row">
-
-
-                    <div className="col-12 mb-3">
-                        <ul className="nav nav-tabs lavalamp" id="myTab" role="tablist">
-                            <li className="nav-item">
-                                <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Related Products</a>
-                            </li>
-                        </ul>
-                    </div>
-
-
                     {mainSection}
-
                 </div>
             </div>
         </section>
