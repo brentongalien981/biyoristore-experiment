942a8e3303a583071e726b920f1e78c4a5bbf3a0
brentongalien981 <brentongalien981@gmail.com>
2021-06-07 10:51:52 -0400

    on DOC: Tasks From Previous Iteration, did TASK: - set the currencies on payment-page
    on DOC: Tasks From Previous Iteration, did TASK:
    - set the currencies on payment-page

--- src/containers/cart/OrderSummary.js
+++ src/containers/cart/OrderSummary.js
@@ -77,7 +77,7 @@ function getOrderSummaryValues(items, shouldCalculateForOrderPage, shouldCalcula
     let shipmentRateFee = shipmentRate?.rate ?? null;
     shipmentRateFee = shipmentRate?.rate ? parseFloat(shipmentRate?.rate) : null;
 
-    if (exchangeRates['CAD-to-USD']) {
+    if (exchangeRates && exchangeRates['CAD-to-USD']) {
         shipmentRateFee = shipmentRateFee * parseFloat(exchangeRates['CAD-to-USD'].rate);
         shipmentRateFee = parseFloat(shipmentRateFee.toFixed(2));
     }
