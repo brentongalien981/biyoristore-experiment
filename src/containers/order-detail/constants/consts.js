e1adb59aea9c49228c4813416df20a35417f3028
brentongalien981 <brentongalien981@gmail.com>
2021-07-01 00:15:49 -0400

    on PROJ: BMD / FEAT: Profile / COMPONENT: Orders, worked on TASK: - handle showing of the order-status with the order’s backend-status of xxx.
    on PROJ: BMD / FEAT: Profile / COMPONENT: Orders, worked on TASK:
    - handle showing of the order-status with the order’s backend-status of xxx.

--- src/containers/order-detail/constants/consts.js
+++ src/containers/order-detail/constants/consts.js
@@ -1,41 +1,123 @@
-// BMD-ON-STAGING: Make sure these values or symmetrical on the backend consts.
+/** ORDER-DISPLAY-STATUSES */
+export const ORDER_DISPLAY_STATUS_ORDER_NO_STATUS = { code: 'ORDER-DS-0000', displayMsg: 'n/a' };
+export const ORDER_DISPLAY_STATUS_ORDER_ERROR = { code: 'ORDER-DS-0001', displayMsg: 'Order Error' };
+export const ORDER_DISPLAY_STATUS_PAYMENT_PROCESSING = { code: 'ORDER-DS-1001', displayMsg: 'Payment Processing' };
+export const ORDER_DISPLAY_STATUS_PAYMENT_RECEIVED = { code: 'ORDER-DS-1002', displayMsg: 'Payment Received' };
+export const ORDER_DISPLAY_STATUS_ORDER_CONFIRMED = { code: 'ORDER-DS-1003', displayMsg: 'Order Confirmed' };
+export const ORDER_DISPLAY_STATUS_ORDER_PROCESSING = { code: 'ORDER-DS-1004', displayMsg: 'Order Processing' };
+export const ORDER_DISPLAY_STATUS_ORDER_DISPATCHED = { code: 'ORDER-DS-1005', displayMsg: 'Order Dispatched' };
+export const ORDER_DISPLAY_STATUS_ORDER_DELIVERED = { code: 'ORDER-DS-1006', displayMsg: 'Order Delivered' };
+export const ORDER_DISPLAY_STATUS_ORDER_CANCELLED = { code: 'ORDER-DS-6001', displayMsg: 'Order Cancelled' };
+export const ORDER_DISPLAY_STATUS_ORDER_BEING_RETURNED = { code: 'ORDER-DS-6002', displayMsg: 'Order Being Returned' };
+export const ORDER_DISPLAY_STATUS_ORDER_BEING_REFUNDED = { code: 'ORDER-DS-6003', displayMsg: 'Order Being Refunded' };
+export const ORDER_DISPLAY_STATUS_ORDER_REFUNDED = { code: 'ORDER-DS-7001', displayMsg: 'Order Refunded' };
+export const ORDER_DISPLAY_STATUS_ORDER_CLOSED = { code: 'ORDER-DS-7002', displayMsg: 'Order Closed' };
+
+
+
+
+// Order Error
+export const PAYMENT_METHOD_NOT_CHARGED = -7001;
+export const INVALID_CART = -8001;
+export const CART_HAS_NO_ITEM = -8002;
+export const INVALID_PAYMENT_METHOD = -8003;
+export const ORDER_FINALIZATION_FAILED = -8004;
+export const ORDER_FINALIZATION_EXCEPTION = -8005;
+export const ORDER_FINALIZATION_INCOMPLETE = -8006;
+
+export const POSSIBLE_DOUBLE_PAYMENT = 9001;
+export const MISSING_STRIPE_PAYMENT_INTENT_LINK = 9002;
+export const CUSTOMER_HAS_TO_BE_REFUNDED = 9003;
+
+
+
+// Payment Processing
+export const WAITING_FOR_PAYMENT = 7000;
+export const PAYMENT_METHOD_VALIDATED = 7001;
+
+export const STRIPE_PAYMENT_INTENT_CREATED = 7101;
+
+export const START_OF_FINALIZING_ORDER_WITH_PREDEFINED_PAYMENT = 8100;
+export const DB_CART_CREATED = 8101;
+export const CACHE_CART_UPDATED_TO_LATEST_VERSION = 8102;
+export const DB_CART_ITEMS_CREATED = 8103;
+
+
+
+// Payment Received
+export const PAYMENT_METHOD_CHARGED = 7002;
+
+export const START_OF_FINALIZING_ORDER = 8000;
+export const VALID_CART = 8001;
+export const CART_HAS_ITEM = 8002;
+export const CART_CHECKEDOUT_OK = 8003;
+
+
+
+// Order Confirmed
 export const ORDER_CREATED = 8006;
+export const ORDER_ITEMS_CREATED = 8007;
+export const INVENTORY_QUANTITIES_UPDATED = 8008;
+export const INVENTORY_ORDER_LIMITS_UPDATED = 8009;
+export const CACHE_CART_RESET_OK = 8010;
+export const ORDER_CONFIRMED = 8011;
+export const ORDER_DETAILS_EMAILED_TO_USER = 8012;
+
+
+
+// Order Processing
+export const PROCESSING_FOR_SHIPMENT = 8013;
+export const BEING_SHIPPED = 8014;
+
+export const EVALUATED_INCOMPLETELY_FOR_PURCHASE = -8301;
+export const PURCHASE_INCOMPLETELY_RECEIVED = -8304;
+
+export const BEING_EVALUATED_FOR_PURCHASE = 8300;
+export const TO_BE_PURCHASED = 8301;
+export const PURCHASED = 8302;
+export const TO_BE_PURCHASE_RECEIVED = 8303;
+export const PURCHASE_RECEIVED = 8304;
+export const IN_STOCK = 8305;
+export const TO_BE_PACKAGED = 8306;
+export const PACKAGED = 8307;
+export const TO_BE_DISPATCHED = 8308;
+
+
+
+// Order Dispatched
+export const DISPATCHED = 8309;
+
+
+
+// Order Delivered
 export const DELIVERED = 8015;
 export const FINALIZED = 8016;
+
+
+
+/** REFUNDS */
+// Order Cancelled
 export const CANCELLED = 666001;
+
+
+
+// Order Being Returned
 export const ORDER_APPLIED_FOR_REFUND = 666002;
+export const ORDER_TO_BE_PICKED_UP_BY_CARRIER_FOR_REFUND = 666003;
+export const ORDER_BEING_RETURNED_FOR_REFUND = 666004;
+export const RETURNED = 666005;
+
+
+
+// Order Being Refunded
+export const PROCESSING_FOR_REFUND = 666006;
+
+
+
+// Order Refunded
 export const REFUNDED = 666007;
+
+
+
+// Order Closed
 export const CLOSED = 666008;
-
-// BMD-FOR-DEBUG
-// DB::table('order_statuses')->insert(['code' => 8006, 'name' => 'ORDER_CREATED', 'readable_name' => 'Order Created']);
-// DB::table('order_statuses')->insert(['code' => 8007, 'name' => 'ORDER_ITEMS_CREATED', 'readable_name' => 'Order Items Created']);
-// DB::table('order_statuses')->insert(['code' => 8008, 'name' => 'INVENTORY_QUANTITIES_UPDATED', 'readable_name' => 'Inventory Quantities Updated']);
-// DB::table('order_statuses')->insert(['code' => 8009, 'name' => 'INVENTORY_ORDER_LIMITS_UPDATED', 'readable_name' => 'Inventory Order Limits Updated']);
-// DB::table('order_statuses')->insert(['code' => 8010, 'name' => 'CACHE_CART_RESET_OK', 'readable_name' => 'Cache Cart Has Been Reset OK']);
-// DB::table('order_statuses')->insert(['code' => 8011, 'name' => 'ORDER_BEING_PROCESSED', 'readable_name' => 'Order Being Processed']);
-// DB::table('order_statuses')->insert(['code' => 8012, 'name' => 'ORDER_DETAILS_EMAILED_TO_USER', 'readable_name' => 'Order Details Emailed To User']);
-
-
-// DB::table('order_statuses')->insert(['code' => 8013, 'name' => 'PROCESSING_FOR_SHIPMENT', 'readable_name' => 'Processing for Shipment']);
-// DB::table('order_statuses')->insert(['code' => 8014, 'name' => 'BEING_SHIPPED', 'readable_name' => 'Order Being Shipped']);
-// DB::table('order_statuses')->insert(['code' => 8015, 'name' => 'DELIVERED', 'readable_name' => 'Order Delivered']);
-// DB::table('order_statuses')->insert(['code' => 8016, 'name' => 'FINALIZED', 'readable_name' => 'Finalized']);
-
-
-// DB::table('order_statuses')->insert(['code' => 666001, 'name' => 'CANCELLED', 'readable_name' => 'Order Cancelled']);
-// DB::table('order_statuses')->insert(['code' => 666002, 'name' => 'ORDER_APPLIED_FOR_REFUND', 'readable_name' => 'Order Applied For Refund']);
-// DB::table('order_statuses')->insert(['code' => 666003, 'name' => 'ORDER_TO_BE_PICKED_UP_BY_CARRIER_FOR_REFUND', 'readable_name' => 'Ordrer to be Picked-up by Carrier for Refund']);
-// DB::table('order_statuses')->insert(['code' => 666004, 'name' => 'ORDER_BEING_RETURNED_FOR_REFUND', 'readable_name' => 'Order Being Returned for Refund']);
-// DB::table('order_statuses')->insert(['code' => 666005, 'name' => 'RETURNED', 'readable_name' => 'Order Returned']);
-// DB::table('order_statuses')->insert(['code' => 666006, 'name' => 'PROCESSING_FOR_REFUND', 'readable_name' => 'Processing for Refund']);
-// DB::table('order_statuses')->insert(['code' => 666007, 'name' => 'REFUNDED', 'readable_name' => 'Refunded']);
-// DB::table('order_statuses')->insert(['code' => 666008, 'name' => 'CLOSED', 'readable_name' => 'Closed']);
-
-
-export const ORDER_DISPLAY_STATUS_CANCELLED = { code: 'ORDER-CONST-666001', displayMsg: 'Cancelled' };
-export const ORDER_DISPLAY_STATUS_CLOSED = { code: 'ORDER-CONST-666008', displayMsg: 'Closed' };
-export const ORDER_DISPLAY_STATUS_BEING_PROCESSED = { code: 'ORDER-CONST-8000', displayMsg: 'Being Processed' };
-export const ORDER_DISPLAY_STATUS_DELIVERED = { code: 'ORDER-CONST-8015', displayMsg: 'Delivered' };
-export const ORDER_DISPLAY_STATUS_BEING_REFUNDED = { code: 'ORDER-CONST-666002', displayMsg: 'Evaluating for Refund' };
-export const ORDER_DISPLAY_STATUS_REFUNDED = { code: 'ORDER-CONST-666007', displayMsg: 'Refunded' };
--- src/containers/profile/Order.js
+++ src/containers/profile/Order.js
@@ -45,26 +45,100 @@ function getOrderStatusNode(status) {
 
 
     switch (c) {
-        case orderConsts.DELIVERED:
-        case orderConsts.FINALIZED:
-            displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_DELIVERED;
-            statusColorClassName = 'order-status sent';
-            break;
-        case orderConsts.CANCELLED:
-            displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_CANCELLED;
+        case orderConsts.PAYMENT_METHOD_NOT_CHARGED:
+        case orderConsts.INVALID_CART:
+        case orderConsts.CART_HAS_NO_ITEM:
+        case orderConsts.INVALID_PAYMENT_METHOD:
+        case orderConsts.ORDER_FINALIZATION_FAILED:
+        case orderConsts.ORDER_FINALIZATION_EXCEPTION:
+        case orderConsts.ORDER_FINALIZATION_INCOMPLETE:
+        case orderConsts.POSSIBLE_DOUBLE_PAYMENT:
+        case orderConsts.MISSING_STRIPE_PAYMENT_INTENT_LINK:
+        case orderConsts.CUSTOMER_HAS_TO_BE_REFUNDED:
+            displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_ORDER_ERROR;
             statusColorClassName = 'order-status canceled';
             break;
-        case orderConsts.CLOSED:
-            displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_CLOSED;
+
+        case orderConsts.WAITING_FOR_PAYMENT:
+        case orderConsts.PAYMENT_METHOD_VALIDATED:
+        case orderConsts.STRIPE_PAYMENT_INTENT_CREATED:
+        case orderConsts.START_OF_FINALIZING_ORDER_WITH_PREDEFINED_PAYMENT:
+        case orderConsts.DB_CART_CREATED:
+        case orderConsts.CACHE_CART_UPDATED_TO_LATEST_VERSION:
+        case orderConsts.DB_CART_ITEMS_CREATED:
+            displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_PAYMENT_PROCESSING;
             break;
-        case orderConsts.REFUNDED:
-            displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_REFUNDED;
+
+        case orderConsts.PAYMENT_METHOD_CHARGED:
+        case orderConsts.START_OF_FINALIZING_ORDER:
+        case orderConsts.VALID_CART:
+        case orderConsts.CART_HAS_ITEM:
+        case orderConsts.CART_CHECKEDOUT_OK:
+            displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_PAYMENT_RECEIVED;
+            break;
+
+        case orderConsts.ORDER_CREATED:
+        case orderConsts.ORDER_ITEMS_CREATED:
+        case orderConsts.INVENTORY_QUANTITIES_UPDATED:
+        case orderConsts.INVENTORY_ORDER_LIMITS_UPDATED:
+        case orderConsts.CACHE_CART_RESET_OK:
+        case orderConsts.ORDER_CONFIRMED:
+        case orderConsts.ORDER_DETAILS_EMAILED_TO_USER:
+            displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_ORDER_CONFIRMED;
+            break;
+
+        case orderConsts.PROCESSING_FOR_SHIPMENT:
+        case orderConsts.BEING_SHIPPED:
+        case orderConsts.EVALUATED_INCOMPLETELY_FOR_PURCHASE:
+        case orderConsts.PURCHASE_INCOMPLETELY_RECEIVED:
+        case orderConsts.BEING_EVALUATED_FOR_PURCHASE:
+        case orderConsts.TO_BE_PURCHASED:
+        case orderConsts.PURCHASED:
+        case orderConsts.TO_BE_PURCHASE_RECEIVED:
+        case orderConsts.PURCHASE_RECEIVED:
+        case orderConsts.IN_STOCK:
+        case orderConsts.TO_BE_PACKAGED:
+        case orderConsts.PACKAGED:
+        case orderConsts.TO_BE_DISPATCHED:
+            displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_ORDER_PROCESSING;
+            break;
+
+        case orderConsts.DISPATCHED:
+            displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_ORDER_DISPATCHED;
+            break;
+
+        case orderConsts.DELIVERED:
+        case orderConsts.FINALIZED:
+            displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_ORDER_DELIVERED;
             statusColorClassName = 'order-status sent';
             break;
+
+        case orderConsts.CANCELLED:
+            displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_ORDER_CANCELLED;
+            break;
+
+        case orderConsts.ORDER_APPLIED_FOR_REFUND:
+        case orderConsts.ORDER_TO_BE_PICKED_UP_BY_CARRIER_FOR_REFUND:
+        case orderConsts.ORDER_BEING_RETURNED_FOR_REFUND:
+        case orderConsts.RETURNED:
+            displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_ORDER_BEING_RETURNED;
+            break;
+
+        case orderConsts.PROCESSING_FOR_REFUND:
+            displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_ORDER_BEING_REFUNDED;
+            break;
+
+        case orderConsts.REFUNDED:
+            displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_ORDER_REFUNDED;
+            statusColorClassName = 'order-status sent';
+            break;
+
+        case orderConsts.CLOSED:
+            displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_ORDER_CLOSED;
+            break;
+
         default:
-            if (c >= orderConsts.ORDER_CREATED && c < orderConsts.DELIVERED) { displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_BEING_PROCESSED; }
-            else if (c >= orderConsts.ORDER_APPLIED_FOR_REFUND && c < orderConsts.REFUNDED) { displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_BEING_REFUNDED; }
-            else { displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_CLOSED; }
+            displayedStatus = orderConsts.ORDER_DISPLAY_STATUS_ORDER_NO_STATUS;
             break;
     }
 
