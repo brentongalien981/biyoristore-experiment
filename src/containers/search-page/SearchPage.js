93b0cac65f600f5b068918618ea07cafa376e24d
odox700 <odox700@gmail.com>
2021-10-15 13:49:13 -0400

    finished UCG-01: user searches products
    finished UCG-01: user searches products

--- src/containers/search-page/SearchPage.js
+++ src/containers/search-page/SearchPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
+import { setSearchPhrase } from '../../actions/search';
 import Bs from '../../bs-library/helpers/Bs';
 
 
@@ -12,14 +14,17 @@ class SearchPage extends React.Component {
 
 
 
-    componentDidMount() {
-        const urlParams = this.props.location.search;
-        const acceptedParams = ['q'];
-        const parsedCleanUrlParams = Bs.getParsedQueryParams(urlParams, acceptedParams);
-        this.setState({ searchPhrase: parsedCleanUrlParams['q'] });
-        Bs.log('parsedCleanUrlParams ==> ...');
-        Bs.log(parsedCleanUrlParams);
+    componentDidUpdate() {
+        this.syncSearchPhraseFromUrlAndRedux();
     }
+
+
+
+    componentDidMount() {
+        this.syncSearchPhraseFromUrlAndRedux();
+    }
+
+
 
     render() {
         return (
@@ -28,7 +33,7 @@ class SearchPage extends React.Component {
                     <div className="row">
                         <div className="col">
                             <h3>Search Page</h3>
-                            <h5>{this.state.searchPhrase}</h5>
+                            <h5>{this.props.searchPhrase}</h5>
                         </div>
                     </div>
 
@@ -37,12 +42,38 @@ class SearchPage extends React.Component {
 
         );
     }
+
+
+
+    syncSearchPhraseFromUrlAndRedux = () => {
+        const urlParams = this.props.location.search;
+        const acceptedParams = ['q'];
+        const parsedCleanUrlParams = Bs.getParsedQueryParams(urlParams, acceptedParams);
+        const urlSearchPhrase = decodeURIComponent(parsedCleanUrlParams['q']);
+
+        if (urlSearchPhrase !== this.props.searchPhrase) {
+            this.props.setSearchPhrase({ searchPhrase: urlSearchPhrase });
+        }
+    };
 }
 
 
 
 /** REACT-FUNCS */
+const mapStateToProps = (state) => {
+    return {
+        searchPhrase: state.search.searchPhrase
+    };
+};
 
 
 
-export default withRouter(SearchPage);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setSearchPhrase: (data) => dispatch(setSearchPhrase(data))
+    };
+};
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(SearchPage));
